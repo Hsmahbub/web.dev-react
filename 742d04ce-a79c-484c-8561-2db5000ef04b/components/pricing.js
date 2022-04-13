8385411671dc612a142b05591837c6dbca3305ab
Hs Mahbub Rohman <87687925+Hsmahbub@users.noreply.github.com>
2022-04-13 06:05:17 +0600

    Delete 742d04ce-a79c-484c-8561-2db5000ef04b/components directory
    the file is not related to this project

--- 742d04ce-a79c-484c-8561-2db5000ef04b/components/pricing.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function pricing({onClick}) {
-  return <div>
-
-  </div>
-}
--- 742d04ce-a79c-484c-8561-2db5000ef04b/components/pricing.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import pricing from './pricing';
-
-export default { title: 'pricing' };
-
-export const storyName = () => <pricing onClick={}/>;
